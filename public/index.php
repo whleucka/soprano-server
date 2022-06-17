d5983a794a336e813aefe4d1691aa68094fd88b0
William Hleucka <william.hleucka@gmail.com>
2022-06-17 12:39:36 -0600

    Update index

--- public/index.php
+++ public/index.php
@@ -1,7 +1,15 @@
 <?php
 
+/**
+ * Welcome to Celestial!
+ * Official repository: https://github.com/libra-php/celestial.git
+ * Created with <3 
+ * Copyright (c) 2022 William Hleucka. All Rights Reserved.
+ */
+
 require_once __DIR__ . "/../vendor/autoload.php";
 
 use Celestial\Kernel\Main;
 
 new Main();
+
