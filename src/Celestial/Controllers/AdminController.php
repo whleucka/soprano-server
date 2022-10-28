6264dabb32b9223e31f66cbd497d57a6d4973d5b
William Hleucka <william.hleucka@gmail.com>
2022-10-28 10:05:55 -0600

    Upgrade

--- src/Celestial/Controllers/AdminController.php
+++ src/Celestial/Controllers/AdminController.php
@@ -45,7 +45,10 @@ class AdminController extends BaseController
         $item->list_align = [
             "id" => "right",
         ];
-        $item->list_filters = ["name"];
+        $item->list_filters = [
+            "name",
+            "email",
+        ];
         $item->list_format = [
             "name" => function ($col, $val) {
                 return "<span style='color: red'>{$val}</span>";
