43d150b70a41aed099dda2471536203138a2bb29
William Hleucka <william.hleucka@gmail.com>
2022-10-28 09:52:33 -0600

    feat: search list table

--- src/Celestial/Controllers/AdminController.php
+++ src/Celestial/Controllers/AdminController.php
@@ -45,6 +45,7 @@ class AdminController extends BaseController
         $item->list_align = [
             "id" => "right",
         ];
+        $item->list_filters = ["name"];
         $item->list_format = [
             "name" => function ($col, $val) {
                 return "<span style='color: red'>{$val}</span>";
