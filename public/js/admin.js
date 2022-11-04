21b0180a7ee5570d8da549ef89e6df7de1dc05f6
William Hleucka <william.hleucka@gmail.com>
2022-11-03 20:44:04 -0600

    fix: sidebar css, relabel filter

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,8 +1,9 @@
 /**
- * Sidebar search
+ * Sidebar filter
  */
+// Vim type search binding
 const searchHotKey = ["/", "Slash"];
-const searchInput = document.getElementById("sidebar-search");
+const searchInput = document.getElementById("sidebar-filter");
 searchInput.addEventListener("input", function (e) {
   const term = e.target.value;
   if (term == searchHotKey[0]) {
@@ -17,10 +18,9 @@ searchInput.addEventListener("input", function (e) {
     link.tabIndex = match ? 0 : -1;
   }
 });
-// Window listener
+// Focus on keypress binding
 addEventListener("keydown", function (e) {
-  // Vim type search binding
-  if (e.code == searchHotKey[1]) {
-    return document.getElementById("sidebar-search").focus();
-  }
+    if (e.code == searchHotKey[1]) {
+        return document.getElementById("sidebar-filter").focus();
+    }
 });
