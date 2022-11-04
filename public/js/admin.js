6da2c03f97d517a01638d02a1f61ac1eb0863ba2
William Hleucka <william.hleucka@gmail.com>
2022-11-03 19:59:48 -0600

    polish sidebar

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,9 +1,14 @@
 /**
  * Sidebar search
  */
+const searchHotKey = ['/', 'Slash'];
 const searchInput = document.getElementById("sidebar-search");
 searchInput.addEventListener("input", function(e) {
     const term = e.target.value;
+    if (term == searchHotKey[0]) {
+        this.value = "";
+        return;
+    }
     const links = document.getElementsByClassName("sidebar-link");
     for (let link of links) {
         const title = link.dataset.title;
@@ -14,5 +19,12 @@ searchInput.addEventListener("input", function(e) {
         link.tabIndex = match
             ? 0
             : -1;
+    }
+});
+// Window listener
+addEventListener("keydown", function(e) {
+    // Vim type search binding
+    if (e.code == searchHotKey[1]) {
+        return document.getElementById("sidebar-search").focus();
     }
 });
