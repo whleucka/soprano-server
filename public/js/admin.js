e065a149af42e470ba5795272996bbba20781176
William Hleucka <william.hleucka@gmail.com>
2022-11-03 20:00:06 -0600

    formatting

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,30 +1,26 @@
 /**
  * Sidebar search
  */
-const searchHotKey = ['/', 'Slash'];
+const searchHotKey = ["/", "Slash"];
 const searchInput = document.getElementById("sidebar-search");
-searchInput.addEventListener("input", function(e) {
-    const term = e.target.value;
-    if (term == searchHotKey[0]) {
-        this.value = "";
-        return;
-    }
-    const links = document.getElementsByClassName("sidebar-link");
-    for (let link of links) {
-        const title = link.dataset.title;
-        const match = title.match(term.trim().toLowerCase())
-        link.style.display = match
-            ? 'block'
-            : 'none';
-        link.tabIndex = match
-            ? 0
-            : -1;
-    }
+searchInput.addEventListener("input", function (e) {
+  const term = e.target.value;
+  if (term == searchHotKey[0]) {
+    this.value = "";
+    return;
+  }
+  const links = document.getElementsByClassName("sidebar-link");
+  for (let link of links) {
+    const title = link.dataset.title;
+    const match = title.match(term.trim().toLowerCase());
+    link.style.display = match ? "block" : "none";
+    link.tabIndex = match ? 0 : -1;
+  }
 });
 // Window listener
-addEventListener("keydown", function(e) {
-    // Vim type search binding
-    if (e.code == searchHotKey[1]) {
-        return document.getElementById("sidebar-search").focus();
-    }
+addEventListener("keydown", function (e) {
+  // Vim type search binding
+  if (e.code == searchHotKey[1]) {
+    return document.getElementById("sidebar-search").focus();
+  }
 });
