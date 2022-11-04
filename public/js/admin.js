3910898c21aaceb638ad156c47d6dc628f7911ba
William Hleucka <william.hleucka@gmail.com>
2022-11-04 16:38:26 -0600

    fix: if only one link in sidebar and enter is pressed, go to url

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,22 +1,43 @@
 /**
  * Sidebar filter
  */
+
 // Vim type search binding
 const searchHotKey = "Slash";
 const searchInput = document.getElementById("sidebar-filter");
+const sidebarFilter = (term) => {
+    const links = document.getElementsByClassName("sidebar-link");
+    let count = 0;
+    let sidebar_links = [];
+    for (let link of links) {
+        const title = link.dataset.title;
+        const match = title.match(term.trim().toLowerCase());
+        link.style.display = match ? "block" : "none";
+        link.tabIndex = match ? 0 : -1;
+        count += match ? 1 : 0;
+        if (match) {
+            sidebar_links.push(link);
+        }
+    }
+    return sidebar_links;
+};
 searchInput.addEventListener("input", function (e) {
-  const term = e.target.value;
-  const links = document.getElementsByClassName("sidebar-link");
-  for (let link of links) {
-    const title = link.dataset.title;
-    const match = title.match(term.trim().toLowerCase());
-    link.style.display = match ? "block" : "none";
-    link.tabIndex = match ? 0 : -1;
-  }
+    const value = e.target.value;
+    sidebarFilter(value);
+});
+searchInput.addEventListener("keyup", function (e) {
+    if (e.code == "Enter") {
+        const value = e.target.value;
+        const sidebar_links = sidebarFilter(value);
+        // Click the filtered link
+        if (sidebar_links.length == 1) {
+            sidebar_links[0].click();
+        }
+    }
 });
 // Focus on keyup binding
 addEventListener("keyup", function (e) {
-  if (e.code == searchHotKey) {
-    return document.getElementById("sidebar-filter").focus();
-  }
+    if (e.code == searchHotKey) {
+        return document.getElementById("sidebar-filter").focus();
+    }
 });
