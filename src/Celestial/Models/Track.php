583adc91cbd36a74a850ca270cb55fc2ca564584
William Hleucka <william.hleucka@gmail.com>
2022-12-10 15:26:48 -0700

    feat: transcode

--- src/Celestial/Models/Track.php
+++ src/Celestial/Models/Track.php
@@ -2,7 +2,10 @@
 
 namespace Celestial\Models;
 
+use Celestial\Config\Application;
 use Constellation\Model\Model;
+use FFMpeg;
+use Exception;
 
 class Track extends Model
 {
@@ -20,17 +23,16 @@ class Track extends Model
                 $this->mime_type !== 'audio/mpeg'
                 ? $this->transcode()
                 : $this->filenamepath;
-            $mime_type = $this->mime_type ?? mime_content_type($file);
             ob_clean();
             header('Content-Description: File Transfer');
             header("Content-Transfer-Encoding: binary");
-            header('Content-Type: ' . $mime_type);
-            header('Content-Disposition: attachment; filename="'.basename($file).'"');
+            header('Content-Type: audio/mpeg');
+            header('Content-Disposition: attachment; filename="' . basename($file) . '"');
             header('Expires: 0');
             header('Accept-Ranges: bytes');
             header('Cache-Control: must-revalidate');
             header('Pragma: public');
-            header('Content-Length: '.filesize($file));
+            header('Content-Length: ' . filesize($file));
             readfile($file);
             exit();
         } else {
@@ -40,8 +42,34 @@ class Track extends Model
 
     public function transcode()
     {
-        // TODO Fix me and transcode via ffmpeg
-        return $this->filenamepath;
+        $storage_dir = Application::$storage["storage_path"] . '/transcode/';
+        $md5_file = $storage_dir . md5($this->filenamepath) . '.mp3';
+        if (!file_exists($storage_dir)) {
+            if (!mkdir($storage_dir)) {
+                error_log('Failed to create transcode directory!');
+            }
+        }
+        if (!file_exists($md5_file)) {
+            $ffmpeg = FFMpeg\FFMpeg::create([
+                'ffmpeg.binaries' => '/usr/bin/ffmpeg',
+                'ffprobe.binaries' => '/usr/bin/ffprobe',
+                'timeout' => 60 * 5,
+                'ffmpeg.threads' => 12,
+            ]);
+            $audio_channels = 2;
+            $bitrate = 160;
+            $audio = $ffmpeg->open($this->filenamepath);
+            $format = new FFMpeg\Format\Audio\Mp3('libmp3lame');
+            $format
+                ->setAudioChannels($audio_channels)
+                ->setAudioKiloBitrate($bitrate);
+            try {
+                $audio->save($format, $md5_file);
+            } catch (Exception $e) {
+                error_log('Transcode error: ' . $e->getMessage());
+            }
+        }
+        error_log("Transcode file: {$md5_file}");
+        return $md5_file;
     }
 }
-
